f522054a3a1319f842bd34e1d0b2c77599a324bf
Sumitdasss <sumitdas46123@gmail.com>
2026-09-25 11:47:28 +0600

    add

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.use(express.urlencoded({ extended: true }));
 
 // ================= ROUTES =================
 app.use("/", router);
-app.use("/api/Facebook" ,router)
+app.use("/facebook" ,router)
 
 
 export default app;

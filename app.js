6a68f61cc097d72ee4a38113d3a6d3e4f19beabc
Sumitdasss <sumitdas46123@gmail.com>
2026-09-25 12:27:38 +0600

    add

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
+import "dotenv/config";
 import express from "express";
 import cors from "cors";
 import DNS from "dns";
-import dotenv from "dotenv";
+
 
 import { connectDB } from "./config/db.js";
 import router from "./routes/index.js";
 
 DNS.setServers(["1.1.1.1", "8.8.8.8"]);
 
-dotenv.config();
+
 
 const app = express();
 
